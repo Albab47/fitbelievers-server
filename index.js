fd1d52f4621845f7a8fd463d545e5834c11b8936
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-06-04 12:29:38 +0600

    Setup server with database

---
+++ index.js
@@ -0,0 +1,53 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+require("dotenv").config();
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.173efa4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+};
+
+// Middlewares
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// Create a MongoClient
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const db = client.db("fitBelievers")
+    const userCollection = db.collection('users');    
+
+
+    // successful connection ping msg
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+// Testing
+app.get("/", (req, res) => {
+  res.send("fitBelievers server is running");
+});
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
